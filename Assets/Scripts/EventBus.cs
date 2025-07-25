063ed7b3be7e134d763ad9763659d9d9e829b044
Dylan Oelke <82289512+TheZBillDyl@users.noreply.github.com>
2025-07-25 16:50:29 -0500

    Visual Effects update

--- Assets/Scripts/EventBus.cs
+++ Assets/Scripts/EventBus.cs
@@ -1,9 +1,11 @@
 using System;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public static class EventBus
 {
     public static Action<GameObject> EnemyDeath;
+    public static Action<bool> Dashing;
     public static Action<GameObject> DashedInto;
     public static Action<Ability, int> AbilityAdded;
     public static Action<Ability, int> AbilityRemoved;

c9a9d1d67694f62433e0a841af765702e36748ac
Brody <brody_horton@hotmail.com>
2025-08-16 21:10:19 -0500

    Interacting with the map exit will reload the scene and generate a new map.

--- Assets/Scripts/Interactions/ExitInteraction.cs
+++ Assets/Scripts/Interactions/ExitInteraction.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent (typeof(Collider))]
 public class ExitInteraction : MonoBehaviour, Interactable {
@@ -6,6 +7,7 @@ public class ExitInteraction : MonoBehaviour, Interactable {
 
     public void Interact() {
         Debug.Log("You have interacted with the exit of the map!");
+        SceneManager.LoadScene("Procedural Generation Dev Scene", LoadSceneMode.Single);
     }
 
     public string GetMessage() {
